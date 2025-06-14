4911dfd422cf949153175a3ab275dbc97e546dd3
Dimas Gómez <dimasx010@gmail.com>
2025-06-14 16:08:40 -0500

    Add Colors and new phrases

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import './App.css';
 import phrases from './data/phrases.json';
+import colors from './data/colors.json';
 
 
 
 function App() {
+  /* Generate random color background */
+  const color = () => {
+    let n = 0
+    n = Math.floor(Math.random() * colors.length)
+    return colors[n]
+  };
+  /* Generate random phrase */
   const phrase = () => {
     let n = 0
     n = Math.floor(Math.random() * phrases.length)
-    console.log("Hola desde la función");
     return phrases[n]
   };
+  /* Declare variables */
   const onePhrase = phrase()
+  const oneColor = color()
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={{ backgroundColor: oneColor }}>
         <h3>"{onePhrase['title']}"</h3>
         <p>- {onePhrase['author']}</p>
         <a href="https://github.com/dimasx010/tu-frase" target="_blank" rel="noreferrer">🔗 Source</a>
